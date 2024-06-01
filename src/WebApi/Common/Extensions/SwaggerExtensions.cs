108cabf04b4e65a92fe06f658407d75608f57ebf
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-01 10:43:15 +0500

    Добавьте файлы проекта.

---
+++ src/WebApi/Common/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using WebApi.Common.Configurations;
+
+namespace WebApi.Common.Extensions
+{
+	public static class SwaggerExtensions
+	{
+		public static IServiceCollection AddSwagger(this IServiceCollection services)
+		{
+			services.AddSwaggerGen();
+			services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+			return services;
+		}
+	}
+}
---
+++ src/WebApi/Program.cs
@@ -0,0 +1,31 @@
+using WebApi.Common.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Adding Swagger. Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwagger();
+
+// Adding ApiVersioning
+builder.Services.AddAPIVersioning();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
