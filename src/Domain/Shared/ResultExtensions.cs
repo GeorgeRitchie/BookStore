8e8af4670a2844582f46f42570ee0a89ea15f1ae
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-01 13:06:48 +0500

    .

---
+++ src/Domain/Shared/ResultExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Shared
+{
+	public static class ResultExtensions
+	{
+		public static Task<Result> ToTask(this Result result) => Task.FromResult(result);
+		public static Task<Result<TValue?>> ToTask<TValue>(this Result<TValue?> result) => Task.FromResult(result);
+	}
+}
