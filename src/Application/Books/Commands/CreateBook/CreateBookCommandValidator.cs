846e0635012c9d517c3fe8e51e33b539a1383644
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 16:23:08 +0500

    Book create command done

---
+++ src/Application/Books/Commands/CreateBook/CreateBookCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces.Services;
+
+namespace Application.Books.Commands.CreateBook
+{
+	public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
+	{
+		public CreateBookCommandValidator(IFileManager fileManager)
+		{
+			RuleFor(i => i.Title).NotEmpty();
+
+			RuleFor(i => i.Price).GreaterThanOrEqualTo(0);
+
+			When(i => i.Image is not null, () =>
+			{
+				RuleFor(i => i.Image).Custom((image, context) =>
+				{
+					if (fileManager.IsPhoto(image) == false)
+						context.AddFailure("Book image must be a valid photo file.");
+
+					if (image.SizeInBytes > Constants.BookImageFileMaxSizeInBytes)
+						context.AddFailure($"Book image file size must be less or equal to {Constants.BookImageFileMaxSizeInBytes} bytes.");
+				});
+			});
+		}
+	}
+}
