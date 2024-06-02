6a01b49c560e90f67c0d2d9ba53c060807672ea1
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 14:53:49 +0500

    Author delete command done

---
+++ src/Application/Authors/Commands/DeleteAuthor/DeleteAuthorCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application.Authors.Commands.DeleteAuthor
+{
+	public class DeleteAuthorCommandValidator : AbstractValidator<DeleteAuthorCommand>
+	{
+		public DeleteAuthorCommandValidator(IAppDb db)
+		{
+			RuleFor(i => i.Id).NotEmpty().CustomAsync(async (id, context, cancellationToken) =>
+			{
+				if (await db.Authors.GetAllAsNoTracking().AnyAsync(c => c.Id == id, cancellationToken) != true)
+					context.AddFailure($"Author with id {id} not found.");
+			});
+		}
+	}
+}
