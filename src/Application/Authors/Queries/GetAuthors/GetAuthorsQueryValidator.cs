f327a06e78d724614abee1075787a5f462162f08
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 15:22:24 +0500

    Get all authors query done

---
+++ src/Application/Authors/Queries/GetAuthors/GetAuthorsQueryValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Models;
+using Application.Common.Validators;
+
+namespace Application.Authors.Queries.GetAuthors
+{
+	public class GetAuthorsQueryValidator : AbstractValidator<GetAuthorsQuery>
+	{
+		public GetAuthorsQueryValidator()
+		{
+			When(o => o.PaginationParams != null, () =>
+			{
+				RuleFor(o => o.PaginationParams).SetValidator(new PaginationParamsValidator()!);
+			});
+
+			When(o => o.PaginationParams == null, () =>
+			{
+				RuleFor(o => o.PaginationParams).Custom((p, context) =>
+				{
+					if (p == null)
+						context.InstanceToValidate.PaginationParams = new PaginationParams();
+				});
+			});
+		}
+	}
+}
