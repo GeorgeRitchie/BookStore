1983ecce967c1537c3203ba5f1dd6a43460c6059
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 16:29:18 +0500

    Book delete command done

---
+++ src/Application/Books/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Books.Commands.DeleteBook
+{
+	// TODO enable when sign in part is done
+	//[Authorize(Roles = $"{Role.SuperAdmin}")]
+	public class DeleteBookCommand : IRequest<Result>
+	{
+		public Guid Id { get; set; }
+	}
+}
