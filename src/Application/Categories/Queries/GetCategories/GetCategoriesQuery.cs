cb39c764bf3c55697e0f4cd2afc663a6357ad07a
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 13:02:01 +0500

    Get all categories query done

---
+++ src/Application/Categories/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.Models;
+
+namespace Application.Categories.Queries.GetCategories
+{
+	public class GetCategoriesQuery : IRequest<PaginatedList<Category>>
+	{
+		public PaginationParams? PaginationParams { get; set; }
+	}
+}
