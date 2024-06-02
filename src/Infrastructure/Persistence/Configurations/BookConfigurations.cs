94687734fccd1b1949b6fdaf9311c9777054613e
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 09:14:24 +0500

    db configured for new domain entities Author Category Book

---
+++ src/Infrastructure/Persistence/Configurations/BookConfigurations.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Persistence.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+	internal sealed class BookConfigurations : IEntityTypeConfiguration<Book>
+	{
+		public void Configure(EntityTypeBuilder<Book> builder)
+		{
+			builder.ToTable(TableNames.Books);
+
+			builder.HasKey(i => i.Id);
+
+			builder.Property(i => i.Id).ValueGeneratedNever();
+
+			builder.Property(i => i.Title).IsRequired();
+
+			builder.HasIndex(i => i.ISBN).IsUnique();
+
+			builder.Property(i => i.Price).HasPrecision(20, 4);
+
+			builder.HasMany(i => i.Categories).WithMany(c => c.Books);
+			builder.HasMany(i => i.Authors).WithMany(a => a.Books);
+		}
+	}
+}
