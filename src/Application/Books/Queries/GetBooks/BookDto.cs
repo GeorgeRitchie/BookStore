4433278e751901c00f44139f1a6fe1d91a51b3d1
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 17:16:58 +0500

    Book get all query done

---
+++ src/Application/Books/Queries/GetBooks/BookDto.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Models;
+
+namespace Application.Books.Queries.GetBooks
+{
+	public class BookDto : IMapWith<Book>
+	{
+		public string Title { get; set; }
+		public string? Image { get; set; }
+		public string? Description { get; set; }
+		public string? ISBN { get; set; }
+		public decimal Price { get; set; }
+
+		public List<EntityMinData> Categories { get; set; }
+		public List<EntityMinData> Authors { get; set; }
+
+		public void Mapping(Profile profile)
+		{
+			profile.CreateMap<Book, BookDto>()
+				.ForMember(dto => dto.Categories,
+						   opt => opt.MapFrom(b => b.Categories
+															.Select(c => new EntityMinData()
+															{
+																Id = c.Id,
+																Title = c.Name,
+															})))
+				.ForMember(dto => dto.Authors,
+						   opt => opt.MapFrom(b => b.Authors
+															.Select(a => new EntityMinData()
+															{
+																Id = a.Id,
+																Title = $"{a.FirstName} {a.LastName}",
+															})));
+		}
+	}
+}
---
+++ src/Application/Books/Queries/GetBooks/GetBooksQueryValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Models;
+using Application.Common.Validators;
+
+namespace Application.Books.Queries.GetBooks
+{
+	public class GetBooksQueryValidator : AbstractValidator<GetBooksQuery>
+	{
+		public GetBooksQueryValidator()
+		{
+			When(o => o.PaginationParams != null, () =>
+			{
+				RuleFor(o => o.PaginationParams).SetValidator(new PaginationParamsValidator()!);
+			});
+
+			When(o => o.PaginationParams == null, () =>
+			{
+				RuleFor(o => o.PaginationParams).Custom((p, context) =>
+				{
+					if (p == null)
+						context.InstanceToValidate.PaginationParams = new PaginationParams();
+				});
+			});
+		}
+	}
+}
