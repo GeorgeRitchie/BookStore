2d7cb0e4f9922366224f94f7ab2c030b1466695c
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 15:36:37 +0500

    Author controller done

---
+++ src/WebApi/Controllers/AuthorController.cs
@@ -0,0 +1,58 @@
+﻿using Application.Authors.Commands.CreateAuthor;
+using Application.Authors.Commands.DeleteAuthor;
+using Application.Authors.Commands.UpdateAuthor;
+using Application.Authors.Queries.GetAuthor;
+using Application.Authors.Queries.GetAuthors;
+using Application.Common.Models;
+using Asp.Versioning;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Common.Extensions;
+using WebApi.Models.RequestModels.AuthorRequestModels;
+
+namespace WebApi.Controllers
+{
+	[ApiController]
+	[ApiVersion("1.0")]
+	[Route("api/{v:apiVersion}/[controller]/[action]")]
+	public class AuthorController : BaseController
+	{
+		[HttpPost]
+		public async Task<IActionResult> Create([FromForm] CreateAuthorDto authorDto)
+		{
+			var command = Mapper.Map<CreateAuthorCommand>(authorDto);
+			var result = await Mediator.Send(command);
+
+			return result.To200Or400Or500Response();
+		}
+
+		[HttpGet]
+		public async Task<Author> Get([FromQuery] GetAuthorQuery query)
+		{
+			return await Mediator.Send(query);
+		}
+
+		[HttpGet]
+		public async Task<PaginatedList<Author>> GetAll([FromQuery] GetAuthorsQuery query)
+		{
+			return await Mediator.Send(query);
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> Update([FromForm] UpdateAuthorDto authorDto)
+		{
+			var command = Mapper.Map<UpdateAuthorCommand>(authorDto);
+			var result = await Mediator.Send(command);
+
+			return result.To200Or400Or500Response();
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> Delete([FromQuery] DeleteAuthorCommand command)
+		{
+			var result = await Mediator.Send(command);
+
+			return result.To200Or400Or500Response();
+		}
+	}
+}
---
+++ src/WebApi/Models/RequestModels/AuthorRequestModels/UpdateAuthorDto.cs
@@ -0,0 +1,21 @@
+﻿using Application.Authors.Commands.UpdateAuthor;
+using AutoMapper;
+
+namespace WebApi.Models.RequestModels.AuthorRequestModels
+{
+	public class UpdateAuthorDto : IMapWith<UpdateAuthorCommand>
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public IFormFile? Photo { get; set; }
+		public string? Description { get; set; }
+
+		public void Mapping(Profile profile)
+		{
+			profile.CreateMap<UpdateAuthorDto, UpdateAuthorCommand>()
+				.ForMember(cmd => cmd.Photo,
+							opt => opt.MapFrom(dto => dto.Photo != null ? new FormFileWrapper(dto.Photo) : null));
+		}
+	}
+}
