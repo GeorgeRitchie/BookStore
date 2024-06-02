a45e9b2c24a2c35a710f6b46996a8932c857022e
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-02 12:52:19 +0500

    Get one category query done

---
+++ src/Application/Categories/Queries/GetCategory/GetCategoryQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application.Categories.Queries.GetCategory
+{
+	public class GetCategoryQueryHandler(IAppDb db) : IRequestHandler<GetCategoryQuery, Category>
+	{
+		public Task<Category> Handle(GetCategoryQuery request, CancellationToken cancellationToken)
+		{
+			return db.Categories.GetAllAsNoTracking().FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken)!;
+		}
+	}
+}
